4ba3e66217fb5fd15bc7e89d8a890fa2f3a5125d
Tanvir Hossen Bappy <tanvirbappy7706@gmail.com>
2023-09-17 13:55:42 +0600

    variable

---
+++ variable.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var developerName string = "Tanvir Hossen Bappy"
+	var platformName = "Web"
+	yearsOfExp := 3
+	fmt.Println(developerName)
+	fmt.Println(platformName)
+	fmt.Println(yearsOfExp)
+}
